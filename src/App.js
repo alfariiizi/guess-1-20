136f6d98b17f47f713768c210d7d01d52d01fed3
alfariiizi <alfariiiziiiii@gmail.com>
2023-02-27 15:25:43 +0700

    Base game logic and also the UI for mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,92 @@
+import { useState } from "react";
+import { randomNumberGenerator } from "./utilities/randomNumberGenerator";
+
 function App() {
+  const [number, setNumber] = useState(randomNumberGenerator(1, 20));
+
+  const [guessNumber, setGuessNumber] = useState(0);
+  const [score, setScore] = useState(20);
+  const [highScore, setHighScore] = useState(0);
+  const [message, setMessage] = useState("Start guessing...");
+  const [isGameStart, setIsGameStart] = useState(true);
+
+  const handleClickAgain = () => {
+    setMessage("Start guessing...");
+    setGuessNumber(0);
+    setNumber(randomNumberGenerator(1, 20));
+    setIsGameStart(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isGameStart) {
+      if (guessNumber > 20 || guessNumber < 1) {
+        setMessage("Please enter the number between 1 and 20");
+      } else if (guessNumber <= 0) {
+        setMessage("You lose...");
+        setIsGameStart(false);
+      } else if (guessNumber > number) {
+        setMessage("Too high!");
+        setScore((prevScore) => --prevScore);
+      } else if (guessNumber < number) {
+        setMessage("Too low!");
+        setScore((prevScore) => --prevScore);
+      } else {
+        // Correct
+        setMessage("Correct!");
+        // update the score
+        if (score > highScore) setHighScore(score);
+        setScore(20); // reset the score
+        setIsGameStart(false);
+      }
+    }
+  };
+
   return (
-    <div className="">
-      <div></div>
-    </div>
+    <main className="h-screen w-full bg-primary-500 font-pixel text-slate-200">
+      {/* Upper section */}
+      <div className="flex flex-wrap-reverse items-center justify-between gap-3 p-2 text-sm">
+        <button
+          onClick={handleClickAgain}
+          className="bg-slate-200 p-2 text-primary-500">
+          Again!
+        </button>
+        <p>(Between 1 - 20)</p>
+      </div>
+
+      {/* Title section */}
+      <section className="my-14 flex justify-center text-center text-xl">
+        <h1>Guess The Number</h1>
+      </section>
+
+      {/* Score section */}
+      <section className="my-11 flex flex-col items-center justify-center text-sm">
+        <div className="flex flex-col gap-2">
+          <div>&#x1f4af; High Score: {highScore}</div>
+          <div>🚀 Current Score: {score}</div>
+        </div>
+      </section>
+
+      <section className="my-5 flex justify-center text-center">
+        <div>{message}</div>
+      </section>
+
+      <form action="" onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center justify-center gap-11">
+          <input
+            type="number"
+            placeholder="1"
+            value={guessNumber}
+            onChange={(e) => setGuessNumber(e.target.value)}
+            className="h-32 w-1/2 max-w-sm border-8 bg-primary-500 text-center text-2xl"
+          />
+          <button type="submit" className="bg-slate-200 p-4 text-primary-500">
+            Check
+          </button>
+        </div>
+      </form>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pixel: '"Press Start 2P"',
+      },
+      colors: {
+        primary: {
+          100: "#d3d3d3",
+          200: "#a7a7a7",
+          300: "#7a7a7a",
+          400: "#4e4e4e",
+          500: "#222222",
+          600: "#1b1b1b",
+          700: "#141414",
+          800: "#0e0e0e",
+          900: "#070707",
+        },
+      },
+    },
   },
   plugins: [],
 };
